ecb86c81a5b1987562caa16d5c25d816c700ddd8
asma20 <jbeli.asma20@gmail.com>
2021-02-18 14:33:40 +0100

    Affichage avec des variables

---
+++ Exercice4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercice4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String variable1 = "Monsieur";
+            String variable2 = "Vincent";
+            
+            int variable3 = 1;
+
+            Console.WriteLine("Bonjour {0} {1} , vous êtes venu nous rendre visite {2} fois" , variable1,variable2,variable3);
+          
+        
+        }
+    }
+}
